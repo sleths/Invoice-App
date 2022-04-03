78073b04cc36c8f7105602d08c9c9985128c7b16
Sera Zenginler <serazenginler@outlook.com>
2022-04-03 12:33:12 +0300

    First Commit

---
+++ src/language/en.ts
@@ -0,0 +1,23 @@
+const en = {
+	sider: {
+		acc: {
+			overview: "Overview",
+			suggestions: "Recommendations",
+			agencies: "Agencies",
+			advertisers: "Advertisers",
+			campaigns: "Campaigns",
+			lineItems: "Line Items",
+			creatives: "Creatives",
+			agreements: "Deals",
+			reports: "Reports",
+			adManagement: "Ad Management",
+			actionsHistory: "Actions History",
+			settingsTools: "Settings & Tools",
+			settings: "Settings",
+			tools: "Tools",
+			transactions: "Transactions",
+		},
+	},
+};
+
+export default en;
---
+++ src/language/tr.ts
@@ -0,0 +1,23 @@
+const tr = {
+	sider: {
+		acc: {
+			overview: "Genel Bakış",
+			suggestions: "Öneriler",
+			agencies: "Ajanslar",
+			advertisers: "Reklamverenler",
+			campaigns: "Kampanyalar",
+			lineItems: "Satır Öğeleri",
+			creatives: "Reklam Öğeleri",
+			agreements: "Anlaşmalar",
+			reports: "Raporlar",
+			adManagement: "Reklam Yönetimi",
+			actionsHistory: "Değişiklik Geçmişi",
+			settingsTools: "Ayarlar & Araçlar",
+			settings: "Ayarlar",
+			tools: "Araçlar",
+			transactions: "İşlemler",
+		},
+	},
+};
+
+export default tr;
